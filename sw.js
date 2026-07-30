37f8dd8a38d4c0549836214d5490530043995c75
dulcejimenezg27-Dev <dulce.jimenezg27@gmail.com>
2026-07-29 22:33:31 -0500

    Segundo Commit

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker — Colegio La Inmaculada PWA */
-const CACHE_NAME = "inmaculada-v4";
+const CACHE_NAME = "inmaculada-v5";
 const ASSETS = [
   "./index.html",
   "./css/styles.css",
